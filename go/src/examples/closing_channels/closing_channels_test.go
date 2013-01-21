a91b8e60a28f3f4a51b5984b34edac6760783fc3
Chetan Gowda <chetan.hs@gmail.com>
2013-01-21 01:33:59 -0800

    [testbox] Go example for closing channels
    Summary: Example to show the closing of channels.
    
    Test Plan:
    ```
    [chetan@ip-10-160-42-191 go]$ bin/gocov test examples/closing_channels/ | tee /tmp/gocov_closing_channels | bin/gocov report
    warning: building out-of-date packages:
      github.com/axw/gocov
    installing these packages with 'go test -i examples/closing_channels/' will speed future tests.
    
    ok    examples/closing_channels 0.007s
    
    examples/closing_channels//closing_channels.go   closingChannels 93.75% (15/16)
    examples/closing_channels/       --------------- 93.75% (15/16)
    
    [chetan@ip-10-160-42-191 go]$ bin/gocov annotate /tmp/gocov_closing_channels
    
     5      func closingChannels() {
     6        jobs := make(chan int)
     7        done := make(chan bool)
     8
     9        go func() {
    10          for {
    11            j, more := <-jobs
    12            if more {
    13              fmt.Println("Received job", j)
    14            } else {
    15              fmt.Println("Received all jobs")
    16              done <- true
    17 MISS         return
    18            }
    19          }
    20        }()
    21
    22        for j := 1; j <= 3; j++ {
    23          jobs <- j
    24          fmt.Println("Sent job", j)
    25        }
    26
    27        close(jobs)
    28        fmt.Println("Sent all jobs")
    29        <-done
    30      }
    ```
    
    Reviewers: tommy.testy
    
    Reviewed By: tommy.testy
    
    Differential Revision: http://dev.chetangowda.com/D27

---
+++ go/src/examples/closing_channels/closing_channels_test.go
@@ -0,0 +1,14 @@
+package closing_channels
+
+func ExampleClosingChannels() {
+	closingChannels()
+	//Output:
+//Sent job 1
+//Received job 1
+//Sent job 2
+//Received job 2
+//Sent job 3
+//Sent all jobs
+//Received job 3
+//Received all jobs
+}
